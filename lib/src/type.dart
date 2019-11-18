c12211d0b4fe44cc8aac9d8747d2c256de245f56
synw <ansterj@gmail.com>
2019-11-18 10:21:36 +0100

    Add utility function

---
+++ lib/src/type.dart
@@ -0,0 +1,16 @@
+dynamic castTypeForValue(dynamic value, Type colType) {
+  dynamic v;
+  switch (colType) {
+    case int:
+      v = value as int;
+      break;
+    case double:
+      v = value as double;
+      break;
+    case DateTime:
+      v = value as DateTime;
+      break;
+    default:
+  }
+  return v;
+}
