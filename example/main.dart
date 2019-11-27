e202bf68caace341330579e87cf88420d755330d
synw <synw@yahoo.com>
2019-11-27 18:11:24 +0100

    Update example

--- example/main.dart
+++ example/main.dart
@@ -1,7 +1,8 @@
 import 'package:df/df.dart';
 
 Future<void> main() async {
-  final df = await DataFrame.fromCsv("dataset/stocks.csv");
+  final df = await DataFrame.fromCsv("dataset/stocks.csv",
+      dateFormat: "MMM dd yyyy", verbose: true);
   df.show();
   print(df.columns);
 }
