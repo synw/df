53b1fabfc33b4381c4569563578c34da39926475
synw <synw@yahoo.com>
2020-04-12 11:39:42 +0200

    Remove old code

--- lib/src/type.dart
+++ lib/src/type.dart
@@ -1,21 +1,3 @@
-/// Get typed data from a value
-dynamic castTypeForValue(dynamic value, Type colType) {
-  dynamic v;
-  switch (colType) {
-    case int:
-      v = value as int;
-      break;
-    case double:
-      v = value as double;
-      break;
-    case DateTime:
-      v = value as DateTime;
-      break;
-    default:
-  }
-  return v;
-}
-
 /// The format to use for timestamps
 enum TimestampFormat {
   /// Timestamp in seconds from epoch
